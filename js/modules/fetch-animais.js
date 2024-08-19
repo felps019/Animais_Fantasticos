858714d3464a62456c468027f0b3a12a749138f2
felps019 <felipedocs313@gmail.com>
2024-08-19 16:45:52 -0300

    fetch-animais-refatorada

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,7 +1,8 @@
 import AnimaNumeros from './anima-numeros.js';
 
 export default function fetchAnimais(url, target) {
-  //cria a div contendo informacoes com o total de animais
+  // Cria a div contendo informações
+  // com o total de animais
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
@@ -9,36 +10,35 @@ export default function fetchAnimais(url, target) {
     return div;
   }
 
-  //Preenche cada animal no DOM
+  // Preenche cada animal no DOM
   const numerosGrid = document.querySelector(target);
   function preencherAnimais(animal) {
     const divAnimal = createAnimal(animal);
     numerosGrid.appendChild(divAnimal);
   }
 
-  //anima os números de cada animal
+  // Anima os números de cada animal
   function animaAnimaisNumeros() {
     const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
     animaNumeros.init();
   }
 
-  //Puxa os animais através de um arquivo json
-  //e cria cada animal utilizando createAnimal
+  // Puxa os animais através de um arquivo json
+  // e cria cada animal utilizando createAnimal
   async function criarAnimais() {
     try {
-      //Fetch espera a resposta e transforma em json
+      // Fetch, espera a resposta e transforma em json
       const animaisResponse = await fetch(url);
-      //Transforma a resposta em json
       const animaisJSON = await animaisResponse.json();
 
-      //Após a transformaçao de json, ativa as funcões de json
-      //para preencher e animar os numeros
+      // Após a transformação de json, ativa as funções
+      // para preencher e animar os números
       animaisJSON.forEach((animal) => preencherAnimais(animal));
       animaAnimaisNumeros();
     } catch (erro) {
       console.log(erro);
     }
   }
+
   return criarAnimais();
-  fetchAnimais('./animaisapi.json');
 }
